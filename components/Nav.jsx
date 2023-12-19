17dd8e349114ba88e68c3f242abd001062660e17
egwilborn <egwilborn@gmail.com>
2023-12-19 12:24:13 -0600

    starts navbar styling

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,16 @@
 "use client";
-
+// import { Link } from "react-router-dom";
 export default function Nav() {
   return (
-    <header>
-      <ul>
-        <li>home</li>
-        <li>user</li>
-      </ul>
-    </header>
+    <div>
+      <header
+        as="nav"
+        className="flex w-full gap-14 justify-center bg-navy text-pink h-fit"
+      >
+        <a href="#">home</a>
+        <a href="#">feed</a>
+        <a href="#">profile</a>
+      </header>
+    </div>
   );
 }
