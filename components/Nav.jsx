d9f53a9c6b88976e8fab2e47ff108d78fc643ca3
egwilborn <egwilborn@gmail.com>
2023-12-27 17:24:47 -0600

    update nav with home link

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
         as="nav"
         className="flex w-full gap-14 justify-center bg-navy text-pink h-fit text-3xl py-3"
       >
-        <a href="#">home</a>
+        <a href="/">home</a>
         <a href="#">feed</a>
         <a href="#">profile</a>
         <Link href="/api/auth">Sign In</Link>
