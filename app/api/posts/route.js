e487c132ff2c63be757c5885d425d5d1258b997f
egwilborn <egwilborn@gmail.com>
2024-01-06 12:27:21 -0600

    fixes api/posts get function

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -3,10 +3,9 @@ import { db } from "@/firebase";
 export async function GET(_) {
   let posts = [];
   try {
-    const res = await db.collection("posts");
-    console.log(res, posts);
+    const res = await db.collection("posts").get();
     res.forEach(function (doc) {
-      posts.push(doc);
+      posts.push({ id: doc.id, info: doc.data() });
     });
     return Response.json({ status: "success", data: posts }, { status: 200 });
   } catch (err) {
