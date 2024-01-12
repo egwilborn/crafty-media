696a98ec8e4d97bd49d6e080ac497394552076a4
egwilborn <egwilborn@gmail.com>
2024-01-11 19:39:11 -0600

    adds function for detching posts

--- app/page.js
+++ app/page.js
@@ -1,5 +1,30 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return <div className="bg-liteGreen">home</div>;
+  const [posts, setPosts] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/posts")
+      .then((res) => res.json())
+      .then((posts) => {
+        setPosts(posts);
+        setLoading(false);
+        console.log(posts);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!posts) return <p>No profile data</p>;
+
+  const allPosts = posts.data.map(function (post) {
+    return (
+      <div>
+        <p>{post.id}</p>
+        <p>{post.info.description}</p>
+      </div>
+    );
+  });
+  return <div>{allPosts}</div>;
 }
