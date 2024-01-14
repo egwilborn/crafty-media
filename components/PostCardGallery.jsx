3ce7efe4ba758435ed42d684ec40e7a0310f878c
egwilborn <egwilborn@gmail.com>
2024-01-13 22:58:05 -0600

    styles postcard gallery and maps post cards

--- components/PostCardGallery.jsx
+++ components/PostCardGallery.jsx
@@ -1,4 +1,11 @@
 import PostCard from "./PostCard";
 export default function PostCardGallery({ posts }) {
-  return <div></div>;
+  let postCards = posts.data.map(function (post) {
+    return <PostCard post={post} />;
+  });
+  return (
+    <div className=" flex gap-y-6 py-6 bg-liteGreen h-screen flex-col justify-start items-center">
+      {postCards}
+    </div>
+  );
 }
