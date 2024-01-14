0fef98643ed8420e7c41b1d249e05a28defcc3a9
egwilborn <egwilborn@gmail.com>
2024-01-13 22:57:36 -0600

    starts styling for post cards

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,4 +1,7 @@
-export default function PostCard() {
-  return;
-  <div></div>;
+export default function PostCard({ post }) {
+  return (
+    <div className="w-6/12 bg-darkGreen h-2/5 rounded-xl">
+      <p className="text-pink">{post.info.description}</p>
+    </div>
+  );
 }
