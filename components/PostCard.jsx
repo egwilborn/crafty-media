36d0e8bef03039578986efe6d24dd1ad62a88162
egwilborn <egwilborn@gmail.com>
2024-01-14 12:10:27 -0600

    styles photo and card size

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,6 +1,7 @@
 export default function PostCard({ post }) {
   return (
-    <div className="w-6/12 bg-darkGreen h-2/5 rounded-xl">
+    <div className="w-5/12 py-8 bg-darkGreen h-full rounded-xl flex flex-col justify-start items-center">
+      <img src={post.info.photo} className="w-96 h-72" />
       <p className="text-pink">{post.info.description}</p>
     </div>
   );
