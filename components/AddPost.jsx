04c167ba56c513a24b23cc8c3f032a010d9944de
egwilborn <egwilborn@gmail.com>
2024-01-17 18:00:50 -0600

    adds and styles add post button

---
+++ components/AddPost.jsx
@@ -0,0 +1,9 @@
+export default function AddPost() {
+  return (
+    <div className="h-fit w-full pb-5 bg-liteGreen flex direction-col items-center justify-center">
+      <button className="h-20 w-60 bg-navy rounded-xl flex direction-col items-center justify-center button hover:bg-hoverNavy cursor-pointer">
+        <h3 className="text-pink text-3xl">+ Add Post</h3>
+      </button>
+    </div>
+  );
+}
