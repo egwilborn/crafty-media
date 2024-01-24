8bb055ffcfebdd6b0add8b85231a94b908a32dd8
egwilborn <egwilborn@gmail.com>
2024-01-23 21:24:12 -0600

    adds in initial form for adding post

--- components/AddPost.jsx
+++ components/AddPost.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 export default function AddPost() {
-  return (
-    <div className="h-fit w-full pb-5 bg-liteGreen flex direction-col items-center justify-center">
-      <button className="h-20 w-60 bg-navy rounded-xl flex direction-col items-center justify-center button hover:bg-hoverNavy cursor-pointer">
-        <h3 className="text-pink text-3xl">+ Add Post</h3>
-      </button>
-    </div>
-  );
+  const [isForm, setForm] = useState(false);
+
+  function handleClick() {
+    setForm(true);
+  }
+
+  if (isForm) {
+    return (
+      <div className="h-fit w-full pb-5 bg-liteGreen flex direction-col items-center justify-center">
+        <div>
+          <form action="">
+            <input type="file" />
+            <label> caption</label>
+            <input type="textbox" />
+          </form>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="h-fit w-full pb-5 bg-liteGreen flex direction-col items-center justify-center">
+        <button
+          onClick={handleClick}
+          className="h-20 w-60 bg-navy rounded-xl flex direction-col items-center justify-center button hover:bg-hoverNavy cursor-pointer"
+        >
+          <h3 className="text-pink text-3xl">+ New Post</h3>
+        </button>
+      </div>
+    );
+  }
 }
