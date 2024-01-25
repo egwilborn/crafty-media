bcb470c1b35265fdd7740f01d1013a2bb1c65709
egwilborn <egwilborn@gmail.com>
2024-01-25 17:59:31 -0600

    styles add post form

--- components/AddPost.jsx
+++ components/AddPost.jsx
@@ -9,22 +9,30 @@ export default function AddPost() {
 
   if (isForm) {
     return (
-      <div className="h-fit w-full pb-5 bg-liteGreen flex direction-col items-center justify-center">
-        <div>
-          <form action="">
-            <input type="file" />
-            <label> caption</label>
-            <input type="textbox" />
+      <div className="h-fit w-full pb-5 bg-liteGreen flex flex-col items-center justify-center gap-5">
+        <div className="bg-navy p-5 rounded-xl w-5/12">
+          <form
+            action=""
+            className="flex flex-col items-center justify-center gap-5"
+          >
+            <input type="file" className="text-pink" />
+            <div>
+              <label className="text-pink"> Caption: </label>
+              <input type="textbox" className="w-8/12" />
+            </div>
+            <button className="bg-liteGreen text-navy text-lg p-2 rounded-lg hover:bg-hoverLiteGreen">
+              Post
+            </button>
           </form>
         </div>
       </div>
     );
   } else {
     return (
-      <div className="h-fit w-full pb-5 bg-liteGreen flex direction-col items-center justify-center">
+      <div className="h-fit w-full pb-5 bg-liteGreen flex flex-col items-center justify-center">
         <button
           onClick={handleClick}
-          className="h-20 w-60 bg-navy rounded-xl flex direction-col items-center justify-center button hover:bg-hoverNavy cursor-pointer"
+          className="h-20 w-60 bg-navy rounded-xl flex flex-col items-center justify-center button hover:bg-hoverNavy cursor-pointer"
         >
           <h3 className="text-pink text-3xl">+ New Post</h3>
         </button>
