53111650e681d7fdfff9c8b061facfffc6348320
egwilborn <egwilborn@gmail.com>
2024-01-26 20:44:04 -0600

    starts the posts POST route

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -15,3 +15,19 @@ export async function GET(_) {
     );
   }
 }
+
+export async function POST(request) {
+  const formData = await request.formData();
+  const data = formData.get("caption");
+  const file = formData.get("file");
+  if (!file) {
+    return Response.json(
+      { status: "error", message: JSON.stringify("No files received") },
+      { status: 400 }
+    );
+  }
+
+  const buffer = Buffer.from(await file.arrayBuffer());
+  console.log(data);
+  return Response.json({ status: "success", data: buffer });
+}
