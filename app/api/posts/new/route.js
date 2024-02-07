181a23e6fc11f6dc6795369cafb6ae2659441583
egwilborn <egwilborn@gmail.com>
2024-02-06 20:57:34 -0600

    starts aws photo upload

--- app/api/posts/new/route.js
+++ app/api/posts/new/route.js
@@ -1,3 +1,5 @@
+import S3 from "aws-sdk/clients/s3";
+
 export async function POST(request) {
   // accessing formdata
   const formData = await request.formData();
@@ -14,6 +16,10 @@ export async function POST(request) {
   // getting the file as a buffer
   const buffer = Buffer.from(await file.arrayBuffer());
   // need to then gather information for putting file in aws bucket
+  const clientS3 = new S3({});
+  const BUCKET_NAME = process.env.BUCKET_NAME;
+  const filePath = `craftymedia/posts/${req.file.originalname}`; //CONSIDER ADDING LOGGEDIN USER ID TO FIlENAME
+  const params = { Bucket: BUCKET_NAME, Key: filePath, Body: req.file.buffer };
   // make the aws request and get back the url for the file
   // then need to put all the Post information into firebase
   return Response.json({ status: "success", data: buffer });
